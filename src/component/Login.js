136798a91083ecadb752702cc4ff527f420591da
DenverArancillo <denver.arancillo@gmail.com>
2019-09-03 21:56:49 +0800

    creating script functionallity

--- src/component/Login.js
+++ src/component/Login.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react'
 
 export default class Login extends Component {
     render() {
+        const { prChange, prSubmit } = this.props;
+
         return (
             <div>
-                
+                <form>
+                    <input type="text" onChange={prChange('username')}></input><br/>
+                    <input type="password" onChange={prChange('password')}></input><br/>
+                    <button type="button" onClick={prSubmit()}>Submit</button>
+                </form>
             </div>
         )
     }
